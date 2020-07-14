f77ea9849055deb8e0c8624dacb09de3dc20d087
Felipe Gaiad <fpgaiad@gmail.com>
2020-07-14 11:14:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/runner/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.runner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pub.devrel.easypermissions.BuildConfig
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
