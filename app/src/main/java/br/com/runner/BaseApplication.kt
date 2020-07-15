c56c4e64c89f7b9dcb36d8a4c6d0a296fe9c8ad2
Felipe Gaiad <fpgaiad@gmail.com>
2020-07-15 14:43:36 -0300

    Add dependency injection structure

---
+++ app/src/main/java/br/com/runner/BaseApplication.kt
@@ -0,0 +1,8 @@
+package br.com.runner
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+}
---
+++ app/src/main/java/br/com/runner/utils/Constants.kt
@@ -0,0 +1,6 @@
+package br.com.runner.utils
+
+object Constants {
+
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
