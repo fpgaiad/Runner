1af9ab3cab8d85d367d7164561a322bc118f13bf
Felipe Gaiad <fpgaiad@gmail.com>
2020-07-15 15:55:15 -0300

    Add repository

---
+++ app/src/main/java/br/com/runner/repository/MainRepository.kt
@@ -0,0 +1,33 @@
+package br.com.runner.repository
+
+import br.com.runner.db.Run
+import br.com.runner.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    private val runDao: RunDAO
+) {
+
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunsSortedByDate() = runDao.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByTimeInMillis() = runDao.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedByAvgSpeed() = runDao.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDao.getAllRunsSortedByCaloriesBurned()
+
+    fun getAllRunsSortedByDistance() = runDao.getAllRunsSortedByDistance()
+
+    fun getTotalAvgSpeed() = runDao.getTotalAvgSpeed()
+
+    fun getTotalDistance() = runDao.getTotalDistance()
+
+    fun getTotalCaloriesBurned() = runDao.getTotalCaloriesBurned()
+
+    fun getTotalTimeInMillis() = runDao.getTotalTimeInMillis()
+
+}
