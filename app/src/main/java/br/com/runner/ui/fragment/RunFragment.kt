9e33d35718412e3d2e63c905c8fab6357a2998f2
Felipe Gaiad <fpgaiad@gmail.com>
2020-07-16 21:21:50 -0300

    Set  request user permissions

--- app/src/main/java/br/com/runner/ui/fragment/RunFragment.kt
+++ app/src/main/java/br/com/runner/ui/fragment/RunFragment.kt
@@ -1,13 +1,72 @@
 package br.com.runner.ui.fragment
 
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import br.com.runner.R
 import br.com.runner.ui.viewmodel.MainViewModel
-import dagger.hilt.android.AndroidEntryPoint
+import br.com.runner.utils.Constants
+import br.com.runner.utils.TrackingUtility
+import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-@AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+
+        if (TrackingUtility.hasLocationPermissions(requireContext())) return
+
+        val rationale =
+            "Você precisa permitir o acesso a sua localização para utilizar este aplicativo."
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                rationale,
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                rationale,
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
--- app/src/main/java/br/com/runner/utils/Constants.kt
+++ app/src/main/java/br/com/runner/utils/Constants.kt
@@ -3,4 +3,7 @@ package br.com.runner.utils
 object Constants {
 
     const val RUNNING_DATABASE_NAME = "running_db"
+
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
 }
